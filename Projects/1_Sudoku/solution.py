d3f335a5b03b5bd5a84b1d34acfd050a69a3adee
Cristian Berrio <cberrio@expertsystem.com>
2020-03-29 17:57:02 +0200

    Added two diagonals constraint in Sudoku proyect

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -7,8 +7,10 @@ column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
 unitlist = row_units + column_units + square_units
 
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+diagonal_1 = [row+col for row,col in zip(rows,cols)]
+diagonal_2 = [row+col for row,col in zip(rows,reversed(cols))]
+unitlist = unitlist + [diagonal_1] + [diagonal_2]
+
 
 
 # Must be called after all units (including diagonals) are added to the unitlist
